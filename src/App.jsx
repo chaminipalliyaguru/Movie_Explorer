ba033d39a81274fbeeac084e37b1ea786c299a3d
chaminipalliyaguru <bimalkachamini@gmail.com>
2025-05-10 07:02:01 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
+import Navbar from './components/Navbar';
 
 function App() {
   
   return (
     <>
       <Router>
+        <Navbar/>
       <Routes>
-        <Route path='/' element={<Login/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
       </Routes>
       </Router>
     </>
